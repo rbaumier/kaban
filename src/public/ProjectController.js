0ba8b14ef0308e8c0dabe33e665e66018699a2bf
Bruel Nicolas <bruel.nicolas85@gmail.com>
2016-04-18 19:31:46 +0200

    Added project creation

--- src/public/ProjectController.js
+++ src/public/ProjectController.js
@@ -1,13 +1,29 @@
-app.controller('ProjectController', function($scope, $http){
-  
+app.controller('ProjectController', function($scope, $http) {
+
   $scope.loadingProjects = true;
 
-  $http({
-    url: "http://localhost:8080/projects",
-    method: "GET"
-  }).then(function(response){
-    console.log(response);
-    $scope.loadingProjects = false;
-    $scope.projects = response.data;
-  })
- })
+  $scope.getProjects();
+
+  $scope.createProject = function(name) {
+    $http({
+      url: "http://localhost:8080/projects",
+      method: "POST",
+      data: {
+        "name": name
+      }
+    }).then(function(response) {
+      $scope.getProjects();
+    })
+  }
+
+  $scope.getProjects = function() {
+    $http({
+      url: "http://localhost:8080/projects",
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      $scope.loadingProjects = false;
+      $scope.projects = response.data;
+    })
+  }
+})
