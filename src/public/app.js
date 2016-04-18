b8a905d284f3dc223997c9c84cb8e9c2ea3743a9
Bruel Nicolas <bruel.nicolas85@gmail.com>
2016-04-18 18:14:10 +0200

    Débug en Gular

--- src/public/app.js
+++ src/public/app.js
@@ -2,15 +2,18 @@ var app = angular.module('kaban', ['ui.router'])
 
 app.config(function($stateProvider, $urlRouterProvider) {
   //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/404");
   //
-  // Now set up the states
-  $stateProvider.html5mode(true);
 
   $stateProvider
     .state('home', {
-      url: "/",
-      templateUrl: "views/project.html"
+      url: "",
+      templateUrl: "views/project.html",
+      controller: "MainController"
     })
+    .state('404', {
+      url: "/404",
+      templateUrl: "views/404.html"
+    })
+
+  // $urlRouterProvider.otherwise("/404");
 });
