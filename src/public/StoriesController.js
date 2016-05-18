cd0274d7b0ec5d138e21e4896968f5ea53a03b11
julien.leroy <julien.leroy9@gmail.com>
2016-05-18 11:53:43 +0200

    check task to done

--- src/public/StoriesController.js
+++ src/public/StoriesController.js
@@ -63,21 +63,39 @@ app.controller('StoriesController', function($scope, $http, $stateParams) {
 
   $scope.toDone = function(story){
 
-    if (confirm('Are you sure you have checked all the DOD items ?')) {
-      $http({
-        url: "http://localhost:8080/projects/" + $stateParams.projectId + "/sprints/" + $stateParams.sprintId + "/stories/" + story.id,
-        method: "PUT",
-        data: {
-          "name": story.name,
-          "valeur_metier": story.valeur_metier,
-          "effort_technique": story.effort_technique,
-          "zone": 'product_done'
+    tasksAreDone(story.id, function(areDone){
+      if(areDone){
+        if (confirm('Are you sure you have checked all the DOD items ?')) {
+          $http({
+            url: "http://localhost:8080/projects/" + $stateParams.projectId + "/sprints/" + $stateParams.sprintId + "/stories/" + story.id,
+            method: "PUT",
+            data: {
+              "name": story.name,
+              "valeur_metier": story.valeur_metier,
+              "effort_technique": story.effort_technique,
+              "zone": 'product_done'
+            }
+          }).then(function(response) {
+            refresh();
+          })
+        } else {  
         }
-      }).then(function(response) {
-        refresh();
-      })
-    } else {  
-    }
+      } else {
+        swal("c'est pas fait négro");
+      }
+    })
+  }
+
+  function tasksAreDone (storyId, f) {
+    $http({
+      url: "http://localhost:8080/projects/" + $stateParams.projectId + "/sprints/" + $stateParams.sprintId + "/stories/" + storyId + "/tasks",
+      method: "GET",
+    }).then(function(response) {
+      debugger
+      f(response.data.every(function(task) {
+        return task.zone === "DONE";
+      }))
+    })
   }
 
   refresh();
